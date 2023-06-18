6fc44154724ff9c7e586a9cfc0214d1e0c3ac98d
SyphiN <35972897+SyphxN@users.noreply.github.com>
2023-06-17 22:15:00 -0400

    Added GUI
    GUI

--- src/main/java/LinkReDirect.java
+++ src/main/java/LinkReDirect.java
@@ -2,15 +2,39 @@ package main.java;
 
 import java.net.URI;
 import java.awt.Desktop;
+import java.io.IOException;
+
 
 public class LinkReDirect {
-    public static void main(String args[]){
-        try {
-            Desktop desktop = java.awt.Desktop.getDesktop();
-            URI oURL = new URI("https://poshmark.com/listing/nike-sweater-great-condition-63e5a302bd66cdb8d06abc43");
-            desktop.browse(oURL);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void main(String args[]) throws IOException{
+        /*String url = "https://underarmour.scene7.com/is/image/Underarmour/PS1345317-866_HF?rp=standard-0pad|pdpMainDesktop&scl=1&fmt=jpg&qlt=85&resMode=sharp2&cache=on,on&bgc=F0F0F0&wid=566&hei=708&size=566,708";
+        String command[] = {"python","from serpapi import GoogleSearch","import json","params = {","\"api_key\": \"eeb332563bfc822f9412cc8db88139c113f2ea9b05ab11dc38a7028c2ebf52a6\",","\"engine\": \"google_lens\",",("\"url\":\"" +url+"\","),"}","search = GoogleSearch(params)","results = search.get_dict()","with open(r'src\\\\main\\\\java\\\\serpAPI.json','w') as f:","\t"+"json.dump(results,f,indent = 4)","exit()"};
+        System.out.println(command);
+        for(int i = 0; i<command.length;i++){
+            System.out.println(command[i]);
+        }   
+        for(int i = 0; i<command.length;i++){
+            Process p = Runtime.getRuntime().exec(command[i]);
+        }    */
+        String url = "https://underarmour.scene7.com/is/image/Underarmour/PS1345317-866_HF?rp=standard-0pad|pdpMainDesktop&scl=1&fmt=jpg&qlt=85&resMode=sharp2&cache=on,on&bgc=F0F0F0&wid=566&hei=708&size=566,708";
+
+String[] command = {
+    "python",
+    "-c",
+    "from serpapi import GoogleSearch; import json; import sys; params = {\"api_key\": \"eeb332563bfc822f9412cc8db88139c113f2ea9b05ab11dc38a7028c2ebf52a6\", \"engine\": \"google_lens\", \"url\": sys.argv[1]}; search = GoogleSearch(params); results = search.get_dict(); with open('src/main/java/serpAPI.json', 'w') as f: json.dump(results, f, indent=4)",
+    url
+};
+
+try {
+    Process p = Runtime.getRuntime().exec(command);
+    p.waitFor(); // Wait for the process to complete
+} catch (IOException e) {
+    e.printStackTrace();
+} catch (InterruptedException e) {
+    e.printStackTrace();
+}
+
+
     }
 }
+
--- src/main/java/serpAPI.py
+++ src/main/java/serpAPI.py
@@ -2,15 +2,20 @@ from serpapi import GoogleSearch
 import json
 import requests
 
-url = "https://lh3.googleusercontent.com/dqtSpM9s9HE6AAoFXarzHujxYJO2u1lid5aXzIWRHbCCMf61g2CYMVibwVxmnWJJJta-GxW7VtSQmCGwhcAJIvc"
-params = {
-  "api_key": "eeb332563bfc822f9412cc8db88139c113f2ea9b05ab11dc38a7028c2ebf52a6",
-  "engine": "google_lens",
-  "url": url
-}
+def main():
+  url = "https://underarmour.scene7.com/is/image/Underarmour/PS1345317-866_HF?rp=standard-0pad|pdpMainDesktop&scl=1&fmt=jpg&qlt=85&resMode=sharp2&cache=on,on&bgc=F0F0F0&wid=566&hei=708&size=566,708"
+  params = {
+    "api_key": "eeb332563bfc822f9412cc8db88139c113f2ea9b05ab11dc38a7028c2ebf52a6",
+    "engine": "google_lens",
+    "url": url
+  }
 
-search = GoogleSearch(params)
-results = search.get_dict()
-with open(r'src\main\java\serpAPI.json','w') as f:
-    json.dump(results,f,indent = 4)
-print(results['visual_matches'])
+  search = GoogleSearch(params)
+  results = search.get_dict()
+  with open(r'src\\main\\java\\serpAPI.json','w') as f:
+      json.dump(results,f,indent = 4)
+  #print(results['visual_matches'])
+
+if __name__ == "__main__":
+    print("Getting from API")
+    main()
